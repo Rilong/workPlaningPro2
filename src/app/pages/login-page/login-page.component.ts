a9d78204d41cb3bf3cecd55c72de0ce7246d2e00
Rilong <romgnatyuk@gmail.com>
2019-11-17 13:31:34 +0200

    Made register message successful

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,16 +1,20 @@
-import {Component, OnInit} from '@angular/core'
+import {Component, OnDestroy, OnInit} from '@angular/core'
 import {FormControl, FormGroup, Validators} from '@angular/forms'
+import {ActivatedRoute, Params} from '@angular/router'
+import {Subscription} from 'rxjs'
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.sass']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
 
   form: FormGroup
+  pSub: Subscription
+  isRegistered = false
 
-  constructor() {
+  constructor(private route: ActivatedRoute) {
   }
 
   ngOnInit() {
@@ -18,10 +22,19 @@ export class LoginPageComponent implements OnInit {
       email: new FormControl('', Validators.required),
       password: new FormControl('', Validators.required)
     })
+    this.pSub = this.route.queryParams.subscribe((params: Params) => {
+      this.isRegistered = params.register
+    })
   }
 
   submit() {
     console.log(this.form)
+  }
+
+  ngOnDestroy(): void {
+    if (this.pSub) {
+      this.pSub.unsubscribe()
+    }
   }
 
 
