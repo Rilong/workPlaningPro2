caff107d5feb67fcba07946ff6b3c4e05c7eb23d
Rilong <romgnatyuk@gmail.com>
2019-11-17 10:46:00 +0200

    Renamed service

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthService} from '../shared/services/auth/auth.service'
+import {UserService} from '../shared/services/user/user.service'
 
 @Component({
   selector: 'app-header',
@@ -8,7 +8,7 @@ import {AuthService} from '../shared/services/auth/auth.service'
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(public authService: AuthService) { }
+  constructor(public authService: UserService) { }
 
   ngOnInit() {
   }
