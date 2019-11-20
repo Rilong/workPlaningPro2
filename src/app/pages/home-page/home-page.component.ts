a9a4259b8edf077c5dae9ff6008af9a136f1dcb0
Rilong <romgnatyuk@gmail.com>
2019-11-20 15:43:30 +0200

    Created modal

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core'
+import {Modal} from 'materialize-css'
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.sass']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  constructor() { }
+  @ViewChild('createProjectModal', {static: false}) modal: ElementRef
+  modalInstance = null
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit(): void {
+    this.modalInstance = Modal.init(this.modal.nativeElement, {
+      endingTop: '50%'
+    })
+  }
+
+  ngOnDestroy(): void {
+    if (this.modalInstance) {
+      this.modalInstance.destroy()
+    }
+  }
+
+  openProjectModal() {
+    this.modalInstance.open()
+  }
 }
