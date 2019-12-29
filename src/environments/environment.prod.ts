b312dbbd1720e5f9f6be2025470dfa67a4da5738
Rilong <romgnatyuk@gmail.com>
2019-12-29 12:49:41 +0200

    Changed server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 import EnvironmentInterface from './environment.interface'
 
 export const environment: EnvironmentInterface = {
-  server_url: 'http://workplaningproserver/api',
+  server_url: 'http://localhost/api',
   server_date_format: 'YYYY-MM-DD HH:mm:ss',
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 import EnvironmentInterface from './environment.interface'
 
 export const environment: EnvironmentInterface = {
-  server_url: 'http://workplaningproserver/api',
+  server_url: 'http://localhost/api',
   server_date_format: 'YYYY-MM-DD HH:mm:ss',
   production: false
 };
